38a653e2cfbc9c1634d1bd287a7f84db81d95eb8
MC-Atom <thepnkprkr@gmail.com>
2022-05-02 06:29:54 -0700

    Can only jump on the ground now

--- ProjectSandstorm/Assets/Resources/Entities/Player/PlayerMovement.cs
+++ ProjectSandstorm/Assets/Resources/Entities/Player/PlayerMovement.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private BoxCollider2D coll;
+
+    [SerializeField] private LayerMask jumpableGround; // Assigned in the editor under "Player"
 
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Hello World!");
         rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -20,8 +24,12 @@ public class PlayerMovement : MonoBehaviour
         rb.velocity = new Vector2(dirX * 7, rb.velocity.y);
         
 
-        if(Input.GetButtonDown("Jump")){
+        if(Input.GetButtonDown("Jump") && isGrounded()){
             rb.velocity = new Vector2(rb.velocity.x ,14f);
         }
+    }
+
+    private bool isGrounded(){
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
     }
 }
